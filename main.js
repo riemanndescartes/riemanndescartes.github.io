50eb6cd8b5bf28ef244a58a47479bf5e5a432321
Kavya Karthik <114900468+thekavyakarthik@users.noreply.github.com>
2024-06-21 23:03:43 +0000

    that's all of dark mode? ryt??

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function pageContainerDark() {
+function toggleDarkMode() {
     var element = document.getElementById("page-container");
     element.classList.toggle("page-container-dark");
+
+    var element = document.header;
+    element.classList.toggle("header-dark");
+
+    var element = document.a;
+    element.classList.toggle("dark-mode-a");
 }
