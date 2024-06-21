00d9bf070aac96d7874d28fb553c2d65b8e8d971
Kavya Karthik <114900468+thekavyakarthik@users.noreply.github.com>
2024-06-21 22:42:08 +0000

    praying mode change works

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function pageContainerDark() {
-    var element = document.page-container;
+    var element = document.getElementById("page-container");
     element.classList.toggle("page-container-dark");
 }
