f7b6e2d36c9957ecacfff3994fee28a56594aae3
Kavya Karthik <114900468+thekavyakarthik@users.noreply.github.com>
2024-06-25 02:36:38 +0000

    life is getting there

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ function toggleDarkMode() {
     var pagecontainer = document.getElementById("page-container");
     pagecontainer.classList.toggle("page-container-dark");
 
-    var header = document.querySelector("header");
-    header.classList.toggle("header-dark");
+    /*var header = document.querySelector("header");
+    header.classList.toggle("header-dark");*/
 
     var alink = document.querySelectorAll("a");
     alink.forEach(function(link) {
